9c5f9b23a5c3e6f0a0790271ad01c332a36379da
Daniel Sager <daniel@sager1.de>
2015-06-18 11:11:51 +0200

    added libxml develop package

--- install_scripts/basic_packages.sh
+++ install_scripts/basic_packages.sh
@@ -1,7 +1,7 @@
 # basic packages
 sudo apt-get install xclip tree git firefox zsh dconf-editor vim-addon-manager \
                      virtualbox vagrant transmission imagemagick mplayer \
-                     gtk-recordmydesktop
+                     gtk-recordmydesktop libxml2-dev
 
 # set zsh as default shell
 chsh -s /bin/zsh
