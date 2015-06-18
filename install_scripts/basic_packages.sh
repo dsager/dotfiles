c05094d49b37cc7e8eebf6dca8321a13cff5b901
Daniel Sager <daniel@sager1.de>
2015-06-18 16:44:19 +0200

    vim folders & base packages

--- install_scripts/basic_packages.sh
+++ install_scripts/basic_packages.sh
@@ -1,7 +1,8 @@
 # basic packages
 sudo apt-get install xclip tree git firefox zsh dconf-editor vim-addon-manager \
                      virtualbox vagrant transmission imagemagick mplayer \
-                     gtk-recordmydesktop libxml2-dev
+                     gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server 
+
 
 # set zsh as default shell
 chsh -s /bin/zsh
