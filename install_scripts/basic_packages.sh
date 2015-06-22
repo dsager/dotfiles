cfb0902257b808eac5a4428397c936756e553fee
Daniel Sager <daniel@sager1.de>
2015-06-22 19:08:22 +0200

    minor things

--- install_scripts/basic_packages.sh
+++ install_scripts/basic_packages.sh
@@ -1,7 +1,8 @@
 # basic packages
 sudo apt-get install xclip tree git firefox zsh dconf-editor vim-addon-manager \
                      virtualbox vagrant transmission imagemagick mplayer \
-                     gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server 
+                     gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
+                     tmux 
 
 
 # set zsh as default shell
