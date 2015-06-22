90c6b94381c0178c984420083f427b665f31eada
Daniel Sager <daniel@sager1.de>
2015-06-22 19:57:51 +0200

    script for general setup

--- install_scripts/setup.sh
+++ install_scripts/setup.sh
@@ -1,10 +1,14 @@
-# basic packages
+## install basic packages
 sudo apt-get install xclip tree git firefox zsh dconf-editor vim-addon-manager \
                      virtualbox vagrant transmission imagemagick mplayer \
                      gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
-                     tmux 
+                     tmux
 
+## configuration
 
 # set zsh as default shell
 chsh -s /bin/zsh
+
+# set desktop scaling to 2 (HiDPI)
+gsettings set org.gnome.desktop.interface scaling-factor 2
 
