23d8f05694684f8152a0b395958954cfff80b7d9
Daniel Sager <daniel@sager1.de>
2015-07-18 02:03:47 +0200

    install atom from ppa

--- install_scripts/atom.sh
+++
@@ -1,3 +0,0 @@
-wget https://atom.io/download/deb -O ~/Downloads/atom.deb
-sudo dpkg --install ~/Downloads/atom.deb
-rm ~/Downloads/atom.deb
--- install_scripts/setup.sh
+++ install_scripts/setup.sh
@@ -1,50 +1,67 @@
 #! /bin/sh
 
-## install basic packages
+# upgrade current system
 sudo apt-get update
-sudo apt-get install xclip tree git firefox zsh dconf-editor vim-addon-manager \
-                     virtualbox vagrant transmission imagemagick mplayer \
-                     gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
-                     tmux laptop-mode-tools bleachbit nodejs-legacy npm gnupg
+sudo apt-get upgrade
+
+## install basic packages
+sudo apt-get install -y xclip tree git firefox zsh dconf-editor vim-addon-manager \
+                        virtualbox vagrant transmission imagemagick mplayer \
+                        gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
+                        tmux laptop-mode-tools bleachbit nodejs-legacy npm gnupg
 
 ## add additional repositories
 
 # caffeine
 sudo add-apt-repository ppa:caffeine-developers/ppa
+
 # linssid
 sudo add-apt-repository ppa:wseverin/ppa
+
 # insync
-wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
+curl -s https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
   | sudo apt-key add -
 echo deb http://apt.insynchq.com/ubuntu trusty non-free \
   | sudo tee /etc/apt/sources.list.d/insync.list
+
 # oracle java
 sudo add-apt-repository ppa:webupd8team/java
+
 # scudcloud (slack)
 sudo apt-add-repository -y ppa:rael-gc/scudcloud
+
 # syncthing
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo deb http://apt.syncthing.net/ syncthing release \
   | sudo tee /etc/apt/sources.list.d/syncthing-release.list
 # syncthing gtk client
 sudo add-apt-repository ppa:nilarimogard/webupd8
+
 # spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 D2C19886
 echo deb http://repository.spotify.com testing non-free \
   | sudo tee /etc/apt/sources.list.d/spotify.list
+
 # updated graphics driver
 sudo add-apt-repository ppa:oibaf/graphics-drivers
+
 # solaar for logitech unifying receiver
 sudo add-apt-repository ppa:daniel.pavel/solaar
+
 # synapse launcher
 sudo add-apt-repository ppa:synapse-core/ppa
+
 # birdie twitter client
 sudo add-apt-repository ppa:birdie-team/stable
 
+# atom text editor
+sudo add-apt-repository ppa:webupd8team/atom
+
 ## install additional packages
 sudo apt-get update
-sudo apt-get install linssid caffeine insync oracle-java8-installer scudcloud \
-                     syncthing syncthing-gtk spotify-client solaar synapse birdie
+sudo apt-get -y install linssid caffeine insync oracle-java8-installer atom \
+                        scudcloud syncthing syncthing-gtk spotify-client solaar \
+                        synapse birdie
 
 ## configuration
 
