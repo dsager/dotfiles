e91bebb0ab778c76fd84803cbcfcab2800b91590
Daniel Sager <daniel@sager1.de>
2015-08-26 23:15:03 +0200

    split base and additional packages

--- scripts/010_base_packages.sh
+++ scripts/010_base_packages.sh
@@ -1,10 +1,8 @@
-#! /bin/sh
-
-# upgrade current system
-sudo apt-get update
-sudo apt-get upgrade
+#!/usr/bin/env sh
 
 ## install basic packages
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y xclip tree git firefox zsh dconf-editor vim-addon-manager \
                         virtualbox vagrant transmission imagemagick mplayer \
                         gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
@@ -12,95 +10,5 @@ sudo apt-get install -y xclip tree git firefox zsh dconf-editor vim-addon-manage
                         geoclue-2.0 redshift redshift-gtk calibre chromium-browser \
                         ppa-purge
 
-## add additional repositories
-
-# caffeine
-sudo add-apt-repository ppa:caffeine-developers/ppa
-
-# linssid
-sudo add-apt-repository ppa:wseverin/ppa
-
-# insync
-curl -s https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
-  | sudo apt-key add -
-echo deb http://apt.insynchq.com/ubuntu trusty non-free \
-  | sudo tee /etc/apt/sources.list.d/insync.list
-
-# oracle java
-sudo add-apt-repository ppa:webupd8team/java
-
-# scudcloud (slack)
-sudo apt-add-repository -y ppa:rael-gc/scudcloud
-
-# syncthing
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo deb http://apt.syncthing.net/ syncthing release \
-  | sudo tee /etc/apt/sources.list.d/syncthing-release.list
-# syncthing gtk client
-sudo add-apt-repository ppa:nilarimogard/webupd8
-
-# spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 D2C19886
-echo deb http://repository.spotify.com testing non-free \
-  | sudo tee /etc/apt/sources.list.d/spotify.list
-
-# heroku toolbelt
-curl -s https://toolbelt.heroku.com/apt/release.key | sudo apt-key add -
-echo "deb http://toolbelt.heroku.com/ubuntu ./" \
-  | sudo tee /etc/apt/sources.list.d/heroku.list
-
-# updated graphics driver
-sudo add-apt-repository ppa:oibaf/graphics-drivers
-
-# solaar for logitech unifying receiver
-sudo add-apt-repository ppa:daniel.pavel/solaar
-
-# synapse launcher
-sudo add-apt-repository ppa:synapse-core/ppa
-
-# birdie twitter client
-sudo add-apt-repository ppa:birdie-team/stable
-
-# hardware sensors indicator
-sudo add-apt-repository ppa:alexmurray/indicator-sensors
-
-# additional icons
-sudo add-apt-repository ppa:cybre/elementaryplus
-
-# feed reader
-sudo add-apt-repository ppa:eviltwin1/feedreader-stable
-
-# uber writer
-sudo add-apt-repository ppa:w-vollprecht/ppa
-
-# vocal podcast player
-sudo apt-add-repository ppa:nathandyer/vocal-stable
-
-# tomato
-sudo apt-add-repository ppa:tomato-team/tomato-stable
-
-# variety wallpaper manager
-sudo add-apt-repository ppa:peterlevi/ppa
-
-## install additional packages
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get -y install linssid caffeine insync oracle-java8-installer \
-                        scudcloud syncthing syncthing-gtk spotify-client \
-                        solaar synapse birdie indicator-sensors elementaryplus \
-                        heroku-toolbelt feedreader uberwriter vocal tomato \
-                        variety
-sudo apt-get autoremove
-sudo apt-get autoclean
-
-
-## configuration
-
 # set zsh as default shell
 chsh -s /bin/zsh
-
-# change scudcloud image
-wget https://d13yacurqjgara.cloudfront.net/users/48487/screenshots/1400899/attachments/203336/slack-3d.png
-sudo dpkg-divert --add --rename --divert /opt/scudcloud/resources/scudcloud.png.real /opt/scudcloud/resources/scudcloud.png
-sudo mv ~/.dotfiles/icons/slack-3d.png /opt/scudcloud/resources/scudcloud.png
-sudo chmod +r /opt/scudcloud/resources/scudcloud.png
---
+++ scripts/011_extra_packages.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env sh
+
+PACKAGES=""
+
+# caffeine
+sudo add-apt-repository ppa:caffeine-developers/ppa
+PACKAGES="${PACKAGES} caffeine"
+
+# linssid
+sudo add-apt-repository ppa:wseverin/ppa
+PACKAGES="${PACKAGES} linssid"
+
+# insync
+curl -s https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
+  | sudo apt-key add -
+echo deb http://apt.insynchq.com/ubuntu trusty non-free \
+  | sudo tee /etc/apt/sources.list.d/insync.list
+PACKAGES="${PACKAGES} insync"
+
+# oracle java
+sudo add-apt-repository ppa:webupd8team/java
+PACKAGES="${PACKAGES} oracle-java8-installer"
+
+# scudcloud (slack)
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+PACKAGES="${PACKAGES} scudcloud"
+
+# syncthing
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo deb http://apt.syncthing.net/ syncthing release \
+  | sudo tee /etc/apt/sources.list.d/syncthing-release.list
+# syncthing gtk client
+sudo add-apt-repository ppa:nilarimogard/webupd8
+PACKAGES="${PACKAGES} syncthing syncthing-gtk"
+
+# spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 D2C19886
+echo deb http://repository.spotify.com testing non-free \
+  | sudo tee /etc/apt/sources.list.d/spotify.list
+PACKAGES="${PACKAGES} spotify-client"
+
+# heroku toolbelt
+curl -s https://toolbelt.heroku.com/apt/release.key | sudo apt-key add -
+echo "deb http://toolbelt.heroku.com/ubuntu ./" \
+  | sudo tee /etc/apt/sources.list.d/heroku.list
+PACKAGES="${PACKAGES} heroku-toolbelt"
+
+# updated graphics driver
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+
+# solaar for logitech unifying receiver
+sudo add-apt-repository ppa:daniel.pavel/solaar
+PACKAGES="${PACKAGES} solaar"
+
+# synapse launcher
+sudo add-apt-repository ppa:synapse-core/ppa
+PACKAGES="${PACKAGES} synapse"
+
+# birdie twitter client
+sudo add-apt-repository ppa:birdie-team/stable
+PACKAGES="${PACKAGES} birdie"
+
+# hardware sensors indicator
+sudo add-apt-repository ppa:alexmurray/indicator-sensors
+PACKAGES="${PACKAGES} indicator-sensors"
+
+# additional icons
+sudo add-apt-repository ppa:cybre/elementaryplus
+PACKAGES="${PACKAGES} elementaryplus"
+
+# feed reader
+sudo add-apt-repository ppa:eviltwin1/feedreader-stable
+PACKAGES="${PACKAGES} feedreader"
+
+# uber writer
+sudo add-apt-repository ppa:w-vollprecht/ppa
+PACKAGES="${PACKAGES} uberwriter"
+
+# vocal podcast player
+sudo apt-add-repository ppa:nathandyer/vocal-stable
+PACKAGES="${PACKAGES} vocal"
+
+# tomato
+sudo apt-add-repository ppa:tomato-team/tomato-stable
+PACKAGES="${PACKAGES} tomato"
+
+# variety wallpaper manager
+sudo add-apt-repository ppa:peterlevi/ppa
+PACKAGES="${PACKAGES} variety"
+
+# math writer
+sudo apt-add-repository ppa:nasc-team/daily
+PACKAGES="${PACKAGES} nasc"
+
+## install additional packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install ${PACKAGES}
+sudo apt-get autoremove
+sudo apt-get autoclean
+
+## CONFIG
+
+# change scudcloud image
+sudo dpkg-divert --add --rename --divert /opt/scudcloud/resources/scudcloud.png.real /opt/scudcloud/resources/scudcloud.png
+sudo ln ~/.dotfiles/icons/slack-3d.png /opt/scudcloud/resources/scudcloud.png
+sudo chmod +r /opt/scudcloud/resources/scudcloud.png
