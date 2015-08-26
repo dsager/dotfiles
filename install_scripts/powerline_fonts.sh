51eeb18bb94e42a837a7ad897ab6128744bd64e8
Daniel Sager <daniel@sager1.de>
2015-08-26 16:33:21 +0200

    shebangs & execute flag

--- install_scripts/powerline_fonts.sh
+++ install_scripts/powerline_fonts.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # install powerline fonts
 mkdir -p ~/src/tools && cd ~/src/tools
 git clone git@github.com:powerline/fonts.git
--- install_scripts/telegram.sh
+++ install_scripts/telegram.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # install telegram
 wget https://com.tdesktop/linux -O ~/Downloads/telegram.tar.xz
 sudo tar -xf ~/Downloads/telegram.tar.xz -C /opt
