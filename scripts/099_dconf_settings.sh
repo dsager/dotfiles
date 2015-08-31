92d15b944fd10c307dd0337833beb0ebb6cc263d
Daniel Sager <daniel@sager1.de>
2015-08-31 15:20:17 +0200

    tweaked settings

--- scripts/099_dconf_settings.sh
+++ scripts/099_dconf_settings.sh
@@ -3,10 +3,7 @@
 echo "
 
 DCONF SETTINGS
-
 "
-
-MONO_FONT="Liberation Mono for Powerline"
 
 echo "- dim screen when idle"
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim true
@@ -25,18 +22,22 @@ gsettings set org.gnome.mutter workspaces-only-on-primary true
 echo "- screenshot folder in dropbox"
 gsettings set org.gnome.gnome-screenshot auto-save-directory 'file:///home/dsager/Dropbox/Screenshots/'
 
-echo "- set keys for window management"
+echo "- set keyboard shortcuts"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Primary>q']"
 gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>m']"
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Primary><Super>Up']"
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>Up']"
 gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Primary><Super>Down']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "<Super>h"
 
-echo "- set desktop scaling to 2 (HiDPI)"
+echo "- compose key"
+gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
+
+echo "- gnome monospace font"
+gsettings set  org.gnome.desktop.interface monospace-font-name "Hack"
+
+echo "- desktop scaling to 2 (HiDPI)"
 gsettings set org.gnome.desktop.interface scaling-factor 2
-
-echo "- show hidden files in file chooser"
-gsettings set org.gtk.settings.file-chooser show-hidden true
 
 echo "- no dynamic workspaces"
 gsettings set org.pantheon.desktop.gala.behavior dynamic-workspaces false
@@ -48,10 +49,8 @@ gsettings set org.pantheon.desktop.gala.behavior hotcorner-topleft 'show-workspa
 echo "- don't show launcher button"
 gsettings set org.pantheon.desktop.wingpanel show-launcher false
 
-echo "- disable single click in files"
+echo "- files settings"
 gsettings set org.pantheon.files.preferences single-click false
-
-echo "- view settings for files"
 gsettings set org.pantheon.files.preferences default-viewmode 'miller_columns'
 gsettings set org.pantheon.files.column-view zoom-level 'small'
 gsettings set org.pantheon.files.icon-view default-zoom-level 'small'
@@ -61,7 +60,7 @@ echo "- terminal settings"
 gsettings set org.pantheon.terminal.settings cursor-color '#839496'
 gsettings set org.pantheon.terminal.settings cursor-shape 'Underline'
 gsettings set org.pantheon.terminal.settings foreground '#94a3a5'
-gsettings set org.pantheon.terminal.settings font "${MONO_FONT}"
+gsettings set org.pantheon.terminal.settings font "Liberation Mono for Powerline"
 gsettings set org.pantheon.terminal.settings palette '#073642:#dc322f:#859900:#b58900:#268bd2:#ec0048:#2aa198:#94a3a5:#586e75:#cb4b16:#859900:#b58900:#268bd2:#d33682:#2aa198:#6c71c4'
 gsettings set org.pantheon.terminal.settings background '#252e32'
 
@@ -70,6 +69,10 @@ gsettings set org.pantheon.scratch.settings style-scheme 'solarized-dark'
 gsettings set org.pantheon.scratch.settings auto-indent false
 gsettings set org.pantheon.scratch.settings plugins-enabled "['folder-manager', 'highlight-word-selection', 'strip-trailing-save', 'filemanager', 'contractor', 'brackets-completion']"
 gsettings set org.pantheon.scratch.settings use-system-font false
-gsettings set org.pantheon.scratch.settings font "${MONO_FONT}"
+gsettings set org.pantheon.scratch.settings font "Hack"
 gsettings set org.pantheon.scratch.settings show-right-margin true
 
+echo "
+done
+
+"
