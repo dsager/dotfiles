35062543c51c909dd83b627b5e5557c71a1ce7c4
Daniel Sager <daniel@sager1.de>
2015-09-05 23:27:30 +0200

    no more bleachbit

--- scripts/010_base_packages.sh
+++ scripts/010_base_packages.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade
 sudo apt-get install -y xclip tree git firefox zsh dconf-editor vim-addon-manager \
                         virtualbox vagrant transmission imagemagick mplayer \
                         gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
-                        tmux laptop-mode-tools bleachbit nodejs-legacy npm gnupg jq \
+                        tmux laptop-mode-tools nodejs-legacy npm gnupg jq \
                         geoclue-2.0 redshift redshift-gtk calibre chromium-browser \
                         ppa-purge network-manager-openvpn
 
