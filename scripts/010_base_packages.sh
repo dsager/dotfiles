6994d62679c99fa486f7d003dd4424e2ea4c8e45
Daniel Sager <daniel@sager1.de>
2015-09-09 16:19:27 +0200

    load indicator

--- scripts/010_base_packages.sh
+++ scripts/010_base_packages.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y xclip tree git firefox zsh dconf-editor vim-addon-manage
                         gtk-recordmydesktop libxml2-dev rpcbind nfs-kernel-server \
                         tmux laptop-mode-tools nodejs-legacy npm gnupg jq \
                         geoclue-2.0 redshift redshift-gtk calibre chromium-browser \
-                        ppa-purge network-manager-openvpn
+                        ppa-purge network-manager-openvpn indicator-multiload
 
 # set zsh as default shell
 chsh -s /bin/zsh
