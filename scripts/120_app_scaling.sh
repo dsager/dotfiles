ae46bc6cb8f36ba56d4e21851634a160d5d7ef4f
Daniel Sager <daniel@sager1.de>
2015-09-18 11:16:25 +0200

    set scale factor for spotify

--- scripts/120_app_scaling.sh
+++ scripts/120_app_scaling.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
 
+# chrome apps
 echo ""
 for i in ~/.local/share/applications/chrome-*.desktop; do
   echo "------- $i"
   sed -i 's/chromium-browser --profile/chromium-browser --force-device-scale-factor=2 --profile/g' $i
 done
+
+# spotify
+i="/usr/share/applications/spotify.desktop"
+if [ -f "$i" ]; then
+  echo "------- $i"
+  sudo sed -i 's/^Exec=spotify/Exec=spotify --force-device-scale-factor=2/g' $i
+fi
+
 echo ""
