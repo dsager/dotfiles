55a492aed38f231c0c1083c5d859bdf5455c673d
Daniel Sager <daniel@sager1.de>
2016-03-07 10:37:38 +0100

    mount data with exec permissions

--- scripts/000_mount_data.sh
+++ scripts/000_mount_data.sh
@@ -5,7 +5,7 @@ sudo mkdir -p /media/data
 # data partition
 grep '/dev/sda3' /etc/fstab > /dev/null
 if [ $? -eq 1 ]; then
-  MOUNT_OPTS="rw,user,auto,owner"
+  MOUNT_OPTS="rw,user,exec,auto,owner"
   echo "/dev/sda3    /media/data    ext4    ${MOUNT_OPTS}    0    2" | \
     sudo tee -a /etc/fstab
   sudo mount /dev/sda3
