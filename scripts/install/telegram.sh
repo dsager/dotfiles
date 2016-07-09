446f684325832f55c4de5b03a0381d9495102baa
Daniel Sager <daniel@sager1.de>
2016-07-09 14:04:52 +0200

    updated telegram install

--- scripts/install/telegram.sh
+++ scripts/install/telegram.sh
@@ -2,5 +2,5 @@
 
 # install telegram
 wget https://tdesktop.com/linux -O ~/Downloads/telegram.tar.xz
-tar -xf ~/Downloads/telegram.tar.xz -C ~/.local/share/applications
+tar -xf ~/Downloads/telegram.tar.xz -C ~/.local/tools
 rm ~/Downloads/telegram.tar.xz
