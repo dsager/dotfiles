41e924df6a77e0e679ef32a0ccab2e2e2804f727
Daniel Sager <daniel@sager1.de>
2016-07-10 11:16:06 +0200

    fix insync install script

--- scripts/install/pkg_insync.sh
+++ scripts/install/pkg_insync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-codename=`source /etc/lsb-release && echo $DISTRIB_CODENAME`
+. /etc/lsb-release
 
 curl -s https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
   | sudo apt-key add -
-echo "deb http://apt.insynchq.com/ubuntu ${codename} non-free contrib" \
+echo "deb http://apt.insynchq.com/ubuntu ${DISTRIB_CODENAME} non-free contrib" \
   | sudo tee /etc/apt/sources.list.d/insync.list
 sudo apt-get update
 sudo apt-get -y install insync
