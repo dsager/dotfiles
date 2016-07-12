5ffa20b8137db0f78177e77d51da9e605b56a972
Daniel Sager <daniel@sager1.de>
2016-07-12 13:32:17 +0200

    install script for datagrip

---
+++ scripts/install/datagrip.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+VERSION="2016.1.1"
+ARCHIVE="datagrip-${VERSION}.tar.gz"
+URL="https://download.jetbrains.com/datagrip/${ARCHIVE}"
+TOOLS_DIR="${HOME}/.local/tools"
+ARCHIVE_LOCAL="${TOOLS_DIR}/${ARCHIVE}"
+
+if [ ! -f ${ARCHIVE_LOCAL} ]; then
+  wget "${URL}" -O ${ARCHIVE_LOCAL}
+fi
+
+tar -xzf ${ARCHIVE_LOCAL} -C ${TOOLS_DIR}
+rm ${ARCHIVE_LOCAL}
