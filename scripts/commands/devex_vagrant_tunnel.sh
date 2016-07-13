978d9968180e1075347e2a58b911d96b5cb2319b
Daniel Sager <daniel@sager1.de>
2016-07-13 23:04:56 +0200

    cmd: extract comment

--- scripts/commands/devex_vagrant_tunnel.sh
+++ scripts/commands/devex_vagrant_tunnel.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-echo "$HOME/.vagrant.d/insecure_private_key"
-
+# Ports
+# 3002 - Front End`
+# 3004 - Grape API`
+# 5432 - Postgres`
+# 6379 - Redis`
+# 9200 - ES`
+# 9250 - ES test cluster`
 ssh vagrant \
   -i $HOME/.vagrant.d/insecure_private_key \
-  -R 3002:127.0.0.1:3002 `# Front End` \
-  -R 3004:127.0.0.1:3004 `# Grape API` \
-  -L 5432:127.0.0.1:5432 `# Postgres` \
-  -L 6379:127.0.0.1:6379 `# Redis` \
-  -L 9200:127.0.0.1:9200 `# ES` \
-  -L 9250:127.0.0.1:9250 `# ES test cluster`
-
+  -R 3002:127.0.0.1:3002 \
+  -R 3004:127.0.0.1:3004 \
+  -L 5432:127.0.0.1:5432 \
+  -L 6379:127.0.0.1:6379 \
+  -L 9200:127.0.0.1:9200 \
+  -L 9250:127.0.0.1:9250 \
