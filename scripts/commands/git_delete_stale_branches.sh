7acf064ba331acb8d42b0d4071af9a675e78712c
Daniel Sager <daniel@sager1.de>
2016-07-13 13:09:50 +0200

    fix stale branch deletion :scream:

--- scripts/commands/git_delete_stale_branches.sh
+++ scripts/commands/git_delete_stale_branches.sh
@@ -2,7 +2,7 @@
 
 EXCLUDED_BRANCHES="master|develop|source|backup|$(git rev-parse --abbrev-ref HEAD)"
 echo "DELETING LOCAL BRANCHES..."
-git branch | egrep -v $EXCLUDED_BRANCHES | xargs git branch -D 
+git branch | egrep -v $EXCLUDED_BRANCHES | xargs git branch -d 
 echo "DELETING REMOTE BRANCHES..."
 git branch -r | grep origin | sed 's/origin\///g' | egrep -v $EXCLUDED_BRANCHES | xargs -I {} sh -c 'git push origin :{}'
 
