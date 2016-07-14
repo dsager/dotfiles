3897958909e912c616bc1429e09fe2e532e1e272
Daniel Sager <daniel@sager1.de>
2016-07-14 12:18:26 +0200

    fix spotify install script

--- scripts/install/pkg_spotify.sh
+++ scripts/install/pkg_spotify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 D2C19886
-echo deb http://repository.spotify.com stable non-free
+echo deb http://repository.spotify.com stable non-free \
   | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get -y install spotify-client
