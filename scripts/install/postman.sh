0f03158a13fc7a8d3a151f769009dfd8bc1f2ffd
Daniel Sager <daniel@sager1.de>
2016-12-06 17:32:44 +0100

    add postman desktop entry

--- scripts/install/postman.sh
+++ scripts/install/postman.sh
@@ -4,3 +4,15 @@
 wget https://dl.pstmn.io/download/latest/linux?arch=64 -O ~/Downloads/postman.tar.xz
 tar -xf ~/Downloads/postman.tar.xz -C ~/.local/tools
 rm ~/Downloads/postman.tar.xz
+
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Postman
+Icon=${HOME}/.local/tools/Postman/resources/app/assets/icon.png
+Exec="${HOME}/.local/tools/Postman/Postman" %f
+Categories=Development;IDE;
+Terminal=false
+EOL
+
