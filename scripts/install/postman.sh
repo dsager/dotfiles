b0eb71206a3cbbdf251c89909e51ac03b28cf656
Daniel Sager <daniel@sager1.de>
2016-12-09 10:24:33 +0100

    add update option for postman

--- scripts/install/postman.sh
+++ scripts/install/postman.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 
+TARGET_FOLDER="${HOME}/.local/tools/Postman"
+
+if [ -d "$TARGET_FOLDER" ]; then
+  if [ "$1" = "update" ]; then
+    rm -rf "$TARGET_FOLDER"
+  else
+    echo "Postman directory exists, run script with command 'update'."
+    exit 1
+  fi
+fi
+
 # install postman
 wget https://dl.pstmn.io/download/latest/linux?arch=64 -O ~/Downloads/postman.tar.xz
 tar -xf ~/Downloads/postman.tar.xz -C ~/.local/tools
@@ -10,8 +21,8 @@ cat > ~/.local/share/applications/postman.desktop <<EOL
 Version=1.0
 Type=Application
 Name=Postman
-Icon=${HOME}/.local/tools/Postman/resources/app/assets/icon.png
-Exec="${HOME}/.local/tools/Postman/Postman" %f
+Icon=${TARGET_FOLDER}/resources/app/assets/icon.png
+Exec="${TARGET_FOLDER}/Postman" %f
 Categories=Development;IDE;
 Terminal=false
 EOL
