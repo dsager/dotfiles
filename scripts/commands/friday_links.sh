9b3b8d8bb0cc89d2842ada5cc28593db8c8a1c77
Daniel Sager <daniel@sager1.de>
2017-01-05 11:08:42 +0100

    allow custom date for friday link script

--- scripts/commands/friday_links.sh
+++ scripts/commands/friday_links.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
+FROM_DATE_STRING=${1:-"1 week ago"}
+FROM_DATE=`date --date="${FROM_DATE_STRING}" +"%Y-%m-%dT00:01:00Z"`
+
 AUTH=`pass web/pinboard | tail -n1`
-FROM_DATE=`date --date="1 week ago" +"%Y-%m-%dT00:01:00Z"`
+
 BASE_URL="https://api.pinboard.in/v1/posts/all"
 URL="${BASE_URL}?format=json&tag=fl&fromdt=${FROM_DATE}&auth_token=${AUTH}"
 
