691f3ed331f03363e9007ca805b486751c3c56d0
Daniel Sager <software@sager1.de>
2017-10-06 18:24:46 +0200

    update rubymine and datagrip install scripts

--- scripts/install/datagrip.sh
+++ scripts/install/datagrip.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-VERSION="2017.1"
+VERSION="2017.2.2"
 ARCHIVE="datagrip-${VERSION}.tar.gz"
 URL="https://download.jetbrains.com/datagrip/${ARCHIVE}"
 TOOLS_DIR="${HOME}/.local/tools"
@@ -11,3 +11,7 @@ fi
 
 tar -xzf ${ARCHIVE_LOCAL} -C ${TOOLS_DIR}
 rm ${ARCHIVE_LOCAL}
+
+# re-set symlink
+rm "${TOOLS_DIR}/DataGrip"
+ln -s "${TOOLS_DIR}/DataGrip-${VERSION}" "${TOOLS_DIR}/DataGrip"
--- scripts/install/rubymine.sh
+++ scripts/install/rubymine.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-VERSION="2017.1"
+VERSION="2017.2.2"
 ARCHIVE="RubyMine-${VERSION}.tar.gz"
 URL="https://download.jetbrains.com/ruby/${ARCHIVE}"
 TOOLS_DIR="${HOME}/.local/tools"
@@ -11,3 +11,7 @@ fi
 
 tar -xzf ${ARCHIVE_LOCAL} -C ${TOOLS_DIR}
 rm ${ARCHIVE_LOCAL}
+
+# re-set symlink
+rm "${TOOLS_DIR}/RubyMine"
+ln -s "${TOOLS_DIR}/RubyMine-${VERSION}" "${TOOLS_DIR}/RubyMine"
