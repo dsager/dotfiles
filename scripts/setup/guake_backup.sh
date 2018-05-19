0f27fe15a2a39d59839c0c9b3dab201579cb9112
Daniel Sager <software@sager1.de>
2018-05-19 19:16:52 +0200

    remove unused guake scripts

--- scripts/setup/guake_backup.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-gconftool-2 --dump /apps/guake > ~/.dotfiles/config-export/guake/apps-guake.xml
-gconftool-2 --dump /schemas/apps/guake > ~/.dotfiles/config-export/guake/schemas-apps-guake.xml
-
