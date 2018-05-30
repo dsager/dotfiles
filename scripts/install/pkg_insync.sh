a4bdde50e5d3bad04f8d52724749821d63a258d0
Daniel Sager <software@sager1.de>
2018-05-30 20:48:23 +0200

    update insync install script

--- scripts/install/pkg_insync.sh
+++ scripts/install/pkg_insync.sh
@@ -6,6 +6,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 
 echo "deb http://apt.insynchq.com/ubuntu ${DISTRIB_CODENAME} non-free contrib" \
   | sudo tee /etc/apt/sources.list.d/insync.list
-sudo apt-get update
-sudo apt-get -y install insync
+
+sudo apt update
+sudo apt -y install insync sni-qt
 
