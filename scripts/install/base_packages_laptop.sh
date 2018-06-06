4ba26c4520499a9ece3d517a7004b434037976c8
Daniel Sager <software@sager1.de>
2018-06-06 11:08:19 +0200

    remove package laptop-mode-tools

--- scripts/install/base_packages_laptop.sh
+++ scripts/install/base_packages_laptop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 sudo apt update
-sudo apt install -y laptop-mode-tools \
-                    tlp \
-                    tlp-rdw \
+sudo apt install -y \
+         tlp \
+         tlp-rdw \
 
 
 sudo tlp start
