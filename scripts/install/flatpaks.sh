39389029f83e7000da52794decd92a039c203605
Daniel Sager <software@sager1.de>
2018-06-06 16:49:43 +0200

    add flatpaks and remove some snaps

--- scripts/install/flatpaks.sh
+++ scripts/install/flatpaks.sh
@@ -1,2 +1,8 @@
-flatpak install flathub com.frac_tion.teleport
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub com.slack.Slack
+flatpak install flathub com.skype.Client
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.github.fabiocolacio.marker
 
--- scripts/install/snap_packages.sh
+++ scripts/install/snap_packages.sh
@@ -1,10 +1,5 @@
 #! /bin/sh
 
-sudo snap install \
-  spotify \
-  telegram-desktop \
-
-sudo snap install --classic slack
 sudo snap install --classic rubymine
 sudo snap install --classic datagrip
 
