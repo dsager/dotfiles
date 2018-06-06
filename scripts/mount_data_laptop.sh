8fadf336285251ab4f6d9a481022404059b35b38
Daniel Sager <software@sager1.de>
2018-06-06 17:19:19 +0200

    update data mount script

--- scripts/mount_data_laptop.sh
+++ scripts/mount_data_laptop.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
+DEVICE="/dev/nvme0n1p2"
+DIR="/media/data"
 
-sudo mkdir -p /media/data
-
-# data partition
-grep '/dev/sda3' /etc/fstab > /dev/null
+grep "${DEVICE}" /etc/fstab > /dev/null
 if [ $? -eq 1 ]; then
+  sudo mkdir -p "${DIR}"
+  sudo chown -R -v dani:dani "${DIR}"
+  sudo chmod -Rf 777 "${DIR}"
+
   MOUNT_OPTS="rw,user,exec,auto,owner"
-  echo "/dev/sda3    /media/data    ext4    ${MOUNT_OPTS}    0    2" | \
+  echo "${DEVICE}    ${DIR}    ext4    ${MOUNT_OPTS}    0    2" | \
     sudo tee -a /etc/fstab
-  sudo mount /dev/sda3
+  sudo mount "${DEVICE}"
 fi
