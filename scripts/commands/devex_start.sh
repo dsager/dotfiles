f9a6e1fd442452305ff9d6f1211f4d8e23a1adc8
Daniel Sager <software@sager1.de>
2018-06-27 17:38:11 +0200

    change devex cfg for docker

--- scripts/commands/devex_start.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-gnome-terminal --tab-with-profile=dani -e "zsh -c 'tmuxinator devex';zsh"
--- scripts/commands/devex_vagrant_https_port.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-sysctl net.ipv4.ip_forward=1
-iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
-iptables -t nat -I OUTPUT -p tcp -o lo --dport 443 -j REDIRECT --to-ports 8443
-iptables -t nat -A POSTROUTING -j MASQUERADE
-
