e3bcb104fb02512641bf38c4582aa1517f91814c
Daniel Sager <software@sager1.de>
2018-09-20 16:51:33 +0200

    update keybase installer

--- scripts/setup/032_keybase.sh
+++ scripts/setup/032_keybase.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-wget https://dist.keybase.io/linux/deb/keybase-latest-amd64.deb \
-  -O ~/Downloads/keybase-latest-amd64.deb
-sudo dpkg -i ~/Downloads/keybase-latest-amd64.deb
+wget https://prerelease.keybase.io/keybase_amd64.deb \
+  -O ~/Downloads/keybase_amd64.deb
+sudo dpkg -i ~/Downloads/keybase_amd64.deb
+sudo apt install -f
+run_keybase
