c338653db867f89651d603b86390f900e93d4c52
Daniel Sager <software@sager1.de>
2018-10-12 19:45:56 +0200

    disable nerd fonts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -59,7 +59,7 @@ set -g theme_title_use_abbreviated_path yes
 set -g theme_date_format "+%a %H:%M"
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
+set -g theme_nerd_fonts no
 set -g theme_show_exit_status yes
 set -g default_user dani
 set -g theme_color_scheme terminal-dark
