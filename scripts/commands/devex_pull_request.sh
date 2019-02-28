5db24a9f626691386cc29d98b478d4368c4bf9fc
Daniel Sager <software@sager1.de>
2019-02-28 11:31:16 +0100

    Add optional target bracnh parameter ghpr scirpt

--- scripts/commands/devex_pull_request.sh
+++ scripts/commands/devex_pull_request.sh
@@ -2,8 +2,9 @@
 
 git push mine
 
+TARGET_BRANCH=${1:-develop}
 REPO=$(git config --get remote.origin.url | sed -e 's/git@github.com://' -e 's/.git$//')
 FORK=$(git config --get remote.mine.url | sed -e 's/.*://' -e 's/\/.*//')
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-xdg-open "https://github.com/${REPO}/compare/develop...${FORK}:${BRANCH}?expand=1"
+xdg-open "https://github.com/${REPO}/compare/${TARGET_BRANCH}...${FORK}:${BRANCH}?expand=1"
