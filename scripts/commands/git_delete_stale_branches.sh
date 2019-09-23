618613fe86dadfbed5ee33d89f84ac52aad15663
Daniel Sager <software@sager1.de>
2019-09-23 08:27:21 +0200

    remove script for deleting git branches

--- scripts/commands/git_delete_stale_branches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-EXCLUDED_BRANCHES="master|develop|source|backup|$(git rev-parse --abbrev-ref HEAD)"
-echo "DELETING LOCAL BRANCHES..."
-git branch | egrep -v $EXCLUDED_BRANCHES | xargs git branch -d 
-echo "DELETING REMOTE BRANCHES..."
-git branch -r | grep origin | sed 's/origin\///g' | egrep -v $EXCLUDED_BRANCHES | xargs -I {} sh -c 'git push origin :{}'
-
