dfd460f409c0a562bf2dff1c5224f50d83bb558a
Daniel Sager <software@sager1.de>
2020-02-07 23:21:58 +0100

    update dotfiles and pass via gws

--- scripts/commands/update_system.sh
+++ scripts/commands/update_system.sh
@@ -14,14 +14,5 @@ flatpak update
 # update fish plugins
 fish -c 'fisher'
 
-# update dotfiles
-cd $HOME/.dotfiles
-if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
-  git pull
-fi
-
-# update password store
-cd $HOME/.password-store
-if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
-  git pull
-fi
+# update dotfiles & password store
+cat $HOME/.projects.gws | /usr/bin/gws
