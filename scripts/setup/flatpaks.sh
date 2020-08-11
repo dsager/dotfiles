09ed482b4e37a31f0cf96d713bb53d067d6c6c8f
Daniel Sager <software@sager1.de>
2020-08-11 12:24:28 +0200

    Install Slack via AUR, not flatpak

--- scripts/setup/flatpaks.sh
+++ scripts/setup/flatpaks.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
 flatpak install flathub com.spotify.Client
-flatpak install flathub com.slack.Slack
 flatpak install flathub com.skype.Client
 flatpak install flathub com.getpostman.Postman
