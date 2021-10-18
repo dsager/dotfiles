8301fa1789151129968ce942ae6489d382f77fb0
Daniel Sager <software@sager1.de>
2021-10-18 09:15:18 +0200

    switch devex branch to master

--- scripts/commands/devex_pull_request.sh
+++ scripts/commands/devex_pull_request.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+set -e -o pipefail
+
 git push mine
 
-TARGET_BRANCH=${1:-develop}
+TARGET_BRANCH=${1:-master}
 REPO=$(git config --get remote.origin.url | sed -e 's/git@github.com://' -e 's/.git$//')
 FORK=$(git config --get remote.mine.url | sed -e 's/.*://' -e 's/\/.*//')
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 xdg-open "https://github.com/${REPO}/compare/${TARGET_BRANCH}...${FORK}:${BRANCH}?expand=1"
+
