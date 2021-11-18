99972158ff4c73dcc8cc6d6891f03a5aa602a29c
Daniel Sager <software@sager1.de>
2021-11-18 15:57:37 +0100

    Add simple mounting for NAS system

---
+++ scripts/commands/mount_nas.sh
@@ -0,0 +1,6 @@
+#!/bin/env sh
+
+NAS_IP="192.168.86.39"
+
+sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/Pictures /home/dani/NAS/Pictures
+sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/Archive /home/dani/NAS/Archive
