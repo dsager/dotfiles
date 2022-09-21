f533d35182ded18a1652a7714d90b9875f35bae4
Daniel Sager <software@sager1.de>
2022-09-21 16:00:39 +0200

    fix NAS folder for videos

--- scripts/commands/mount_nas.sh
+++ scripts/commands/mount_nas.sh
@@ -4,4 +4,4 @@ NAS_IP="192.168.86.39"
 
 sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/Pictures /home/dani/NAS/Pictures
 sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/Archive /home/dani/NAS/Archive
-sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/video /home/dani/NAS/Videos
+sudo mount -t nfs -o nfsvers=4 $NAS_IP:/volume1/Videos /home/dani/NAS/Videos
