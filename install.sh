67e6a74ec11e7e9f91287e17f3b054eb942e7937
Daniel Sager <software@sager1.de>
2023-07-28 17:21:47 +0200

    Add setup for Code

--- install.sh
+++ install.sh
@@ -35,25 +35,27 @@ sudo cp "${BASEDIR}/config/etc/sysctl.d/99-sysctl.conf" /etc/sysctl.d/99-sysctl.
 
 # link dotfiles, tools & config
 printf "\n======================================================================\nSET UP SYMLINKS\n"
-ln -sT "${BASEDIR}/gitignore_global"                          "${HOME}/.gitignore_global"                 || true
-ln -sT "${BASEDIR}/gitconfig"                                 "${HOME}/.gitconfig"                        || true
-ln -sT "${BASEDIR}/gitconfig_devex"                           "${HOME}/.gitconfig_devex"                  || true
-ln -sT "${BASEDIR}/wgetrc"                                    "${HOME}/.wgetrc"                           || true
-ln -sT "${BASEDIR}/curlrc"                                    "${HOME}/.curlrc"                           || true
-ln -sT "${BASEDIR}/psqlrc"                                    "${HOME}/.psqlrc"                           || true
-ln -sT "${BASEDIR}/vimrc"                                     "${HOME}/.vimrc"                            || true
-ln -sT "${BASEDIR}/irbrc"                                     "${HOME}/.irbrc"                            || true
-ln -sT "${BASEDIR}/npmrc"                                     "${HOME}/.npmrc"                            || true
-ln -sT "${BASEDIR}/irbrc"                                     "${HOME}/.pryrc"                            || true
-ln -sT "${BASEDIR}/src/projects.gws"                          "${HOME}/.projects.gws"                     || true
-ln -sT "${BASEDIR}/scripts/commands/update_system.sh"         "${HOME}/.local/bin/update"                 || true
-ln -sT "${BASEDIR}/scripts/commands/friday_links.sh"          "${HOME}/.local/bin/friday_links"           || true
-ln -sT "${BASEDIR}/scripts/commands/devex_pull_request.sh"    "${HOME}/.local/bin/ghpr"                   || true
-ln -sT "${BASEDIR}/scripts/commands/mount_nas.sh"             "${HOME}/.local/bin/mount_nas"              || true
-ln -sT "${BASEDIR}/config/fish/config.fish"                   "${HOME}/.config/fish/config.fish"          || true
-ln -sT "${BASEDIR}/config/fish/fishfile"                      "${HOME}/.config/fish/fishfile"             || true
-ln -sT "${BASEDIR}/config/ulauncher"                          "${HOME}/.config/ulauncher"                 || true
-ln -sT "${HOME}/Dropbox/notes/idid.log"                       "${HOME}/.idid.log"                         || true
+ln -sT "${BASEDIR}/gitignore_global"                          "${HOME}/.gitignore_global"                           || true
+ln -sT "${BASEDIR}/gitconfig"                                 "${HOME}/.gitconfig"                                  || true
+ln -sT "${BASEDIR}/gitconfig_devex"                           "${HOME}/.gitconfig_devex"                            || true
+ln -sT "${BASEDIR}/wgetrc"                                    "${HOME}/.wgetrc"                                     || true
+ln -sT "${BASEDIR}/curlrc"                                    "${HOME}/.curlrc"                                     || true
+ln -sT "${BASEDIR}/psqlrc"                                    "${HOME}/.psqlrc"                                     || true
+ln -sT "${BASEDIR}/vimrc"                                     "${HOME}/.vimrc"                                      || true
+ln -sT "${BASEDIR}/irbrc"                                     "${HOME}/.irbrc"                                      || true
+ln -sT "${BASEDIR}/npmrc"                                     "${HOME}/.npmrc"                                      || true
+ln -sT "${BASEDIR}/irbrc"                                     "${HOME}/.pryrc"                                      || true
+ln -sT "${BASEDIR}/src/projects.gws"                          "${HOME}/.projects.gws"                               || true
+ln -sT "${BASEDIR}/scripts/commands/update_system.sh"         "${HOME}/.local/bin/update"                           || true
+ln -sT "${BASEDIR}/scripts/commands/friday_links.sh"          "${HOME}/.local/bin/friday_links"                     || true
+ln -sT "${BASEDIR}/scripts/commands/devex_pull_request.sh"    "${HOME}/.local/bin/ghpr"                             || true
+ln -sT "${BASEDIR}/scripts/commands/mount_nas.sh"             "${HOME}/.local/bin/mount_nas"                        || true
+ln -sT "${BASEDIR}/config/Code/settings.json"                 "${HOME}/.config/Code - OSS/User/settings.json"       || true
+ln -sT "${BASEDIR}/config/Code/keybindings.json"              "${HOME}/.config/Code - OSS/User/keybindings.json"    || true
+ln -sT "${BASEDIR}/config/fish/config.fish"                   "${HOME}/.config/fish/config.fish"                    || true
+ln -sT "${BASEDIR}/config/fish/fishfile"                      "${HOME}/.config/fish/fishfile"                       || true
+ln -sT "${BASEDIR}/config/ulauncher"                          "${HOME}/.config/ulauncher"                           || true
+ln -sT "${HOME}/Dropbox/notes/idid.log"                       "${HOME}/.idid.log"                                   || true
 
 printf "\n======================================================================\nCONFIGURE GNOME\n"
 "${BASEDIR}/scripts/setup/gsettings.sh"
@@ -72,4 +74,3 @@ sudo pamac install ${PACKAGES}
 printf "\n======================================================================\nBUILD AUR PACKAGES\n"
 PACKAGES=$(pamac list --explicitly-installed --quiet | sort | comm -13 - "${BASEDIR}/packages/aur.txt" | grep -v '#')
 echo "${PACKAGES}" | xargs pamac build --no-confirm
-
---
+++ scripts/setup/code-install_extensions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+code --install-extension adam-bender.commit-message-editor --force &
+code --install-extension castwide.solargraph --force &
+code --install-extension DavidAnson.vscode-markdownlint --force &
+code --install-extension dbaeumer.vscode-eslint --force &
+code --install-extension eamodio.gitlens --force &
+code --install-extension EditorConfig.EditorConfig --force &
+code --install-extension esbenp.prettier-vscode --force &
+code --install-extension formulahendry.code-runner --force &
+code --install-extension GitHub.vscode-pull-request-github --force &
+code --install-extension kaiwood.endwise --force &
+code --install-extension Lourenci.go-to-spec --force &
+code --install-extension mhutchie.git-graph --force &
+code --install-extension ms-azuretools.vscode-docker --force &
+code --install-extension rebornix.ruby --force &
+code --install-extension redhat.vscode-yaml --force &
+code --install-extension streetsidesoftware.code-spell-checker --force &
+code --install-extension wingrunr21.vscode-ruby --force &
+code --install-extension yzhang.markdown-all-in-one --force
